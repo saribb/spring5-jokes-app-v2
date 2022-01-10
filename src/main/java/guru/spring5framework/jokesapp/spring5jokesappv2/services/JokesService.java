73ce50bb457f37e9391f347b203740aaaa5a2a29
saribb <sari.barhum@gmail.com>
2022-01-10 23:35:21 +0200

    Implement Service

---
+++ src/main/java/guru/spring5framework/jokesapp/spring5jokesappv2/services/JokesService.java
@@ -0,0 +1,6 @@
+package guru.spring5framework.jokesapp.spring5jokesappv2.services;
+
+public interface JokesService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/spring5framework/jokesapp/spring5jokesappv2/services/JokesServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.spring5framework.jokesapp.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
